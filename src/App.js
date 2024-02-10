f75f745c81a93580d2e333a44b97543791e1bd67
unknown <lalwanid998@gmail.com>
2024-02-11 00:38:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    componentDidMount() {
+        if (localStorage.getItem("uname")) {
+            var a = localStorage.getItem("uname");
+            this.setState({ msg: "Data is " + a});
+        }
+    }
+    saveData() {
+        var a =  this.state.txt1;
+        localStorage.setItem("uname", a);
+        this.setState({ msg: "Data Saved" });
+    }
+    getData() {
+        if (localStorage.getItem("uname")) {
+            var b = localStorage.getItem("uname");
+            this.setState({ msg: "Hi," + b});
+        }else{
+            this.setState({ msg: "Data is Not Available"});
+        }
+    }
+    removeData() {
+        localStorage.removeItem("uname");
+        this.setState({ msg: "Data Removed"});
+    }
+    render() {
+        return(
+            <>
+                <h1>Local Storage</h1>
+                {this.state.msg}<br />
+                Name : <input type="text" onChange={(e) => this.setState({ txt1: e.target.value})} />
+                <button onClick={this.saveData.bind(this)}>Save</button>
+                <button onClick={this.getData.bind(this)}>Get</button>
+                <button onClick={this.removeData.bind(this)}>remove</button>
+            </>
+        );
+    }
 }
-
 export default App;
